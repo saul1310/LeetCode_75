1c995e543eb387810726d391731476b7b2cd3921
unknown <stifshin@willamette.edu>
2024-06-18 10:44:24 -0700

    june 18

---
+++ max-consecutive-ones.py
@@ -0,0 +1,10 @@
+def longestOnes():
+        mx,l =0,0
+        for r,n in enumerate(nums):
+            k -= 1 - n
+            if k < 0:
+                k += 1 -nums[l] 
+                l +=1
+            mx =max(mx, r- l +1)
+        return mx
+            
